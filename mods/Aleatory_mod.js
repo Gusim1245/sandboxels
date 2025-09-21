451315c8bb534793ee20f21eb32a1838d4f08bd9
Gusim1245 <Gufavo.bpv@gmail.com>
2025-09-20 23:20:42 -0300

    Create Aleatory_mod.js
    aleatory. blue, red, purple.

---
+++ mods/Aleatory_mod.js
@@ -0,0 +1,32 @@
+elements.azul = {
+    color: ["#0000FF"],   
+    behavior: [
+        "   | ^ |   ",     
+        "   | X |   ",
+        "   | X |   "
+    ],
+    state: "gas",
+    density: 0.5
+};
+
+elements.vermelho = {
+    color: ["#FF0000"],   
+    behavior: [
+        "   | v |   ",     
+        "   | X |   ",
+        "   | X |   "
+    ],
+    state: "gas",
+    density: 0.5
+};
+
+elements.roxo = {
+    color: ["#800080"],   
+    behavior: [
+        "XXX|XXX|XXX",     
+        "XXX|XXX|XXX",
+        "XXX|XXX|XXX"
+    ],
+    state: "solid",
+    density: 1
+};
