be1641c8a872a669ebd463cba22e85ce54efa1e2
Gusim1245 <Gufavo.bpv@gmail.com>
2026-01-04 12:02:06 -0300

    Update enabledMods to include meu_mod12002433

---
+++ mods/meu_mod12002433.js
@@ -0,0 +1,13 @@
+enabledMods ??= [];
+
+enabledMods.push("meu_mod12002433");
+
+elements.meu_elemento = {
+    color: "#ff00ff",
+    behavior: behaviors.WALL,
+    category: "special",
+    state: "solid",
+    tick(pixel) {
+        // roda todo frame
+    }
+};
